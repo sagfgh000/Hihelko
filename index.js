789150bb79c6c36f86d5c38cf62a1892eb8b4d88
MD RIFAT MOHSIN TAPADER <rifatmohsin716@gmail.com>
2025-12-30 15:42:55 +0600

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Bot is running!');
+});
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = require('@whiskeysockets/baileys');
 const pino = require('pino');
 const fs = require('fs-extra');
